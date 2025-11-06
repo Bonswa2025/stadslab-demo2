40108ee0acfbd1b20ff85a9c6daa406ef04e5708
Bonswa2025 <weshley@gmail.com>
2025-11-06 23:32:56 +0100

    Update StadslabBackoffice.tsx

--- components/stadslab/StadslabBackoffice.tsx
+++ components/stadslab/StadslabBackoffice.tsx
@@ -1210,7 +1210,7 @@ setConcepts((prev) =>
         people,
         pct: Math.round((people / total) * 100),
         color: colors[idx % colors.length],
-         });
+         );
   }, [activeConceptIds, instancesById, totalPeople, concepts]);
   return (
   <div
